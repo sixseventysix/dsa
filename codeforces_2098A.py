d5f7ed5feacef6be47dbd053fe73cd92a3011932
samarth <samarthkulkarni@icloud.com>
2025-04-27 19:12:31 +0530

    codeforces 2098A

---
+++ codeforces_2098A.py
@@ -0,0 +1,16 @@
+t = int(input())
+for _ in range(t):
+    s = input()
+    count = [0] * 10
+    for ch in s:
+        count[int(ch)] += 1
+
+    answer = []
+    for i in range(10):
+        lower_bound = 10 - i - 1  # since i is 0-based
+        for d in range(lower_bound, 10):
+            if count[d] > 0:
+                answer.append(str(d))
+                count[d] -= 1
+                break
+    print(''.join(answer))

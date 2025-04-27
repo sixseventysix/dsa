39793e1dc5be94a7b207c6a14e1694f3c86dd719
samarth <samarthkulkarni@icloud.com>
2025-04-27 21:50:18 +0530

    simplified 2106B

--- cf_2106B.py
+++ cf_2106B.py
@@ -3,11 +3,8 @@ for _ in range(t):
     n, x = map(int, input().split())
     answer = []
     val = 0
-    for _ in range(n):
-        if(val == x):
-            val+=1
-            continue
-        answer.append(val)
-        val+=1
-    answer.append(x)
-    print(*answer)
+    for _x in range(x):
+        print(_x, end = " ")
+    for x_ in range(x+1,n):
+        print(x_, end = " ")
+    print(x)
